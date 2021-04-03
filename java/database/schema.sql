4de8fd7294c95d12d1ebc01b5d6cc139b1af854e
Student <student@Techs-Air.attlocal.net>
2021-04-03 09:34:03 -0400

    starting project

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -22,4 +22,15 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 
+
+//WE START
+
+DROP TABLE IF EXISTS art_pieces;
+
+
+CREATE TABLE art_pieces (
+        art_id serial primary key
+);
+
+
 COMMIT TRANSACTION;
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  <div id="app">
+    <div v-if="!image">
+    <h2>Select an image</h2>
+    <input type="file" @change="onFileChange">
   </div>
+  <div v-else>
+    <img :src="image" />
+    <button @click="removeImage">Remove image</button>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "home"
-};
+  name: "home",
+  data() {
+    return {
+      image: ''
+    }
+
+  },
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (e) {
+      this.image = '';
+    }
+  }
+
+  }
+
 </script>
+
+<style scoped>
+#app {
+  text-align: center;
+}
+img {
+  width: 30%;
+  margin: auto;
+  display: block;
+  margin-bottom: 10px;
+}
+</style>
