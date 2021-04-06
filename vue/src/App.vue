105b8d172a58a2b5e0bb902c520d3401c983b86d
Trevor Hughes-Smith <trevorhughes8@yahoo.com>
2021-04-06 13:37:11 -0400

    logo sizing

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/Infinity ART (1).png" alt="logo"/>
-      <div>
-    <div id="nav" class="sidenav">
-      <router-link id="home" v-bind:to="{ name: 'home' }">Home</router-link>
-      <router-link id="login" v-bind:to="{ name: 'login' }">Login</router-link>
-      <router-link id="addart" v-bind:to="{ name: 'AddArt' }"
-        >Create Listing</router-link
-      >
-      <router-link
-        id="logout"
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != ''"
-        >Logout</router-link
-      >
+    <img src="./assets/Infinity ART (1).png" alt="logo" />
+    <div>
+      <div id="nav" class="sidenav">
+        <router-link id="home" v-bind:to="{ name: 'home' }">Home</router-link>
+        <router-link id="login" v-bind:to="{ name: 'login' }"
+          >Login</router-link
+        >
+        <router-link id="addart" v-bind:to="{ name: 'AddArt' }"
+          >Create Listing</router-link
+        >
+        <router-link
+          id="logout"
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token != ''"
+          >Logout</router-link
+        >
+      </div>
+      <router-view />
     </div>
-    <router-view />
-  </div>
   </div>
 </template>
+
+<style scoped>
+img {
+  width: 150px;
+  padding: 20px;
+}
+</style>
