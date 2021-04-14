26e4cca9c25102451ec02d8ed2fcb36b4d168f88
Student <student@Techs-Air.attlocal.net>
2021-04-14 13:41:56 -0400

    Finished tag and finished search function

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,9 +1,43 @@
 <template>
   <div>
     <h1>Featured Art</h1>
+    <div class="searchFunctions">
+      <div>
+        <label for="">Search By Artist: </label>
+        <input type="text" v-model="artistSelection"/>
+      </div>
+      <div id="dropdownMenu">
+        <label for="tag">Choose a Art Tag: </label>
+        <select name="tag" id="tag" v-model="tagSelection">
+          <option value="0">All</option>
+          <option value="2">Abstract</option>
+          <option value="3">Figurative</option>
+          <option value="4">Geometric</option>
+          <option value="5">Minimalist</option>
+          <option value="6">Nature</option>
+          <option value="7">Pop</option>
+          <option value="8">Surrealist</option>
+          <option value="9">Typography</option>
+          <option value="10">Urban</option>
+          <option value="11">Cubism</option>
+          <option value="1">Other</option>
+        </select>
+      </div>
+      <div id="dropdownMenu">
+        <label for="type">Choose a Art Type: </label>
+        <select name="type" id="type" v-model="typeSelection">
+          <option value="0">All</option>
+          <option value="1">Drawing</option>
+          <option value="2">Painting</option>
+          <option value="3">Sculpture</option>
+          <option value="4">Photography</option>
+          <option value="5">Digital</option>
+        </select>
+      </div>
+    </div>
     <div class="art-container">
       <display-art class="art-card"
-        v-for="artPiece in $store.state.artPieceData"
+        v-for="artPiece in getTypeSelection"
         v-bind:key="artPiece.artID"
         v-bind:artPiece="artPiece"
       />
@@ -17,6 +51,14 @@ import artPieceService from "@/services/ArtPieceService.js";
 
 export default {
   name: "home",
+  data(){
+    return{
+      ArtPieces: this.$store.state.artPieceData,
+      tagSelection: 0,
+      typeSelection: 0,
+      artistSelection: ""
+    }
+  },
   components: {
     DisplayArt,
   },
@@ -28,6 +70,28 @@ export default {
       })
       .catch((err) => console.error(err));
   },
+  computed: {
+    getTypeSelection(){
+      let filteredArtPiece = this.ArtPieces;
+      if(this.typeSelection > 0){
+        filteredArtPiece = filteredArtPiece.filter((x) => {
+          return x.typeID == this.typeSelection;
+        });
+      }
+
+      if(this.tagSelection > 0){
+        filteredArtPiece = filteredArtPiece.filter((x) => {
+          return x.tagID == this.tagSelection;
+        });        
+      }
+      if(this.artistSelection != ""){
+        filteredArtPiece = filteredArtPiece.filter((x) => {
+          return x.artist.toUpperCase().includes(this.artistSelection.toUpperCase());
+        });
+      }
+      return filteredArtPiece;
+    }
+  }
 };
 </script>
 
@@ -47,6 +111,18 @@ export default {
   margin: 30px;
   margin-top: 0;
   width: 400px;
+}
+
+.searchFunctions{
+  display: flex;
+  justify-content: center;
+  margin: 10px 0px;
+  color: whitesmoke;
+  font-weight: bold;
+}
+
+#dropdownMenu{
+  margin-left: 20px;
 }
 
 </style>
